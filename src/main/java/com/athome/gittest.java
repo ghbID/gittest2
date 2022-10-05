078ebb6cd6bb2b856d13964c3f41eedbdb660ae4
myname <myemail@qq.com>
2022-10-05 15:21:11 +0800

    first commit

---
+++ src/main/java/com/athome/gittest.java
@@ -0,0 +1,12 @@
+package com.athome;
+
+/**
+ * @author Nikki
+ * @create 2022-10-05 14:59
+ */
+public class gittest {
+
+    public static void main(String[] args) {
+        System.out.println("hello first commit");
+    }
+}
