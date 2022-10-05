5c82f24a675248b94a46e923599a488cc9952014
myname <myemail@qq.com>
2022-10-05 21:40:59 +0800

    7th push gitee commit

--- src/main/java/com/athome/gittest.java
+++ src/main/java/com/athome/gittest.java
@@ -12,6 +12,7 @@ public class gittest {
         System.out.println("hello 3th commit");
         System.out.println("hello 4th commit");
         System.out.println("hello 5th gitHub commit");
-        System.out.println("hello 6th gitee commit");
+        System.out.println("hello 6th pull gitee commit");
+        System.out.println("hello 7th push gitee commit");
     }
 }
